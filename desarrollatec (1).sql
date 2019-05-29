276863f0d73338bc83825a8e2e1b6eaf697f9a5f
ArkhamBoy14 <39526527+ArkhamBoy14@users.noreply.github.com>
2019-05-29 12:57:14 -0500

    Primera subida

--- desarrollatec (1).sql
+++ desarrollatec (1).sql
@@ -1,32 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2019 a las 09:09:49
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "-05:00";
+drop database if exists desarrollatec;
+create database desarrollatec;
+use desarrollatec;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `desarrollatec`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `accessos_rol`
---
 
 CREATE TABLE `accessos_rol` (
   `Modulo_URL` int(11) NOT NULL,
@@ -219,7 +199,3 @@ ALTER TABLE `cat_materias`
 ALTER TABLE `cat_usuarios`
   ADD CONSTRAINT `fk_CAT_USUARIOS_CAT_USUARIOS_ROL` FOREIGN KEY (`Cve_Rol`) REFERENCES `cat_usuarios_rol` (`Cve_Rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
